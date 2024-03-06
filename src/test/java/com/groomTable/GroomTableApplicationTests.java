88b4a1a48eb6ecd491f458a0af71c07e441327c5
shimUnBo <pencer118@gmail.com>
2024-03-06 19:07:37 +0900

    Initial commit

---
+++ src/test/java/com/groomTable/GroomTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groomTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
