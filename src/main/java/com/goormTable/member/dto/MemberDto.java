20a6d1a7eda1ed6f4ca0b83511d958083becae5c
leejuno <zhqkfxm0127@naver.com>
2024-03-12 17:26:24 +0900

    feat: member DTO 작성

--- src/main/java/com/goormTable/member/dto/MemberDto.java
+++ src/main/java/com/goormTable/member/dto/MemberDto.java
@@ -1,4 +1,21 @@
 package com.goormTable.member.dto;
 
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class MemberDto {
+    private Long memberSeq;
+    private String adminId;
+    private String password;
+    private String adminYn;
+    private LocalDateTime reg_time;
 }
