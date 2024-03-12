a67354a0d46ec895fc10b00b4798d27317e3e73a
leejuno <zhqkfxm0127@naver.com>
2024-03-12 10:38:36 +0900

    feat(entity): member엔티티 작성

--- src/main/java/com/groomTable/member/entity/Member.java
+++ src/main/java/com/groomTable/member/entity/Member.java
@@ -1,4 +1,42 @@
 package com.groomTable.member.entity;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "MemberTable")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_seq", nullable = false, unique = true)
+    private Long memberSeq;
+
+    @Column(name = "admin_id")
+    private String adminId;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "admin_yn")
+    private String adminYn;
+
+    @Column(name = "reg_time")
+    private LocalDateTime reg_time;
 }
+
