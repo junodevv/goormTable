8046eb66c2215c94f5e7d1fe9ef33e0257b3ff40
leejuno <zhqkfxm0127@naver.com>
2024-03-14 11:05:40 +0900

    refactor(dto/ReservationDto): member를 memberSeq로 수정

--- src/main/java/com/goormTable/member/dto/ReservationDto.java
+++ src/main/java/com/goormTable/member/dto/ReservationDto.java
@@ -1,9 +1,11 @@
 package com.goormTable.member.dto;
 
 import com.goormTable.member.entity.Member;
+import com.goormTable.member.entity.Reservation;
 import com.goormTable.member.entity.Status;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,12 +16,25 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Builder
 public class ReservationDto {
-    private Long reserveSeq;
+    private Integer reserveSeq;
     private LocalDateTime reservationTime;
-    private int peopleNum;
+    private Integer peopleNum;
     private String phoneNum;
     private String extra;
-    private Status status;
-    private Member member;
+    private String status;
+    private Integer memberSeq;
+
+    public static ReservationDto toDto(Reservation entity){
+        return ReservationDto.builder()
+                .reserveSeq(entity.getReserveSeq())
+                .reservationTime(entity.getReservationTime())
+                .peopleNum(entity.getPeopleNum())
+                .phoneNum(entity.getPhoneNum())
+                .extra(entity.getExtra())
+                .status(entity.getStatus())
+                .memberSeq(entity.getMember().getMemberSeq())
+                .build();
+    }
 }
