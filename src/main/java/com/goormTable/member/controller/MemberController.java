616568050d0b7913b41bab21a0a2523f33c0abee
leejuno <zhqkfxm0127@naver.com>
2024-03-15 20:57:46 +0900

    refactor(MemberController): ResponseEntity 사용

--- src/main/java/com/goormTable/member/controller/MemberController.java
+++ src/main/java/com/goormTable/member/controller/MemberController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,18 +20,18 @@ public class MemberController {
 
     @Operation(summary = "날짜별 손님 조회", description = "날짜별 손님 목록을 반환합니다.")
     @GetMapping("/admin") // admin?company_id=null&day=null
-    public List<ReservationDto> findListByDate(
+    public ResponseEntity<List<ReservationDto>> findListByDate(
             @RequestParam("company_id") String companyId,
             @RequestParam String day
     ){
-        return memberService.findAllByCompanyIdAndDay(companyId, day);
+        return ResponseEntity.ok(memberService.findAllByCompanyIdAndDay(companyId, day));
     }
 
     @Operation(summary = "대기중 손님 수 조회", description = "대기중인 손님의 수를 반환합니다.")
     @GetMapping("/waitingCnt") // watingCnt?company_id=null
-    public Integer waitingCnt(@RequestParam("company_id") String companyId){
+    public ResponseEntity<Integer> waitingCnt(@RequestParam("company_id") String companyId){
 
-        return memberService.getCountByMember(companyId);
+        return ResponseEntity.ok(memberService.getCountByMember(companyId));
     }
 }
 
