35cbe70093dda148160adfb7ab04f221a1063598
kitewater <97283971+kitewater@users.noreply.github.com>
2024-03-19 03:08:28 +0900

    feat : 확인 기능 개발
    유저를 확인해서 status를 confirm 으로 바꾸는 로직 개발

---
+++ src/main/java/com/goormTable/confirm/dto/ConfirmResponse.java
@@ -0,0 +1,14 @@
+/**
+package com.goormTable.confirm.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ConfirmResponse {
+    @NotNull
+    private int code;
+}
+**/
