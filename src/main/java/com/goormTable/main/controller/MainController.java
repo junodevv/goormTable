f1e38aa70b95083d013111c47ba2d7ddd6a85138
shimUnBo <pencer118@gmail.com>
2024-03-21 15:41:23 +0900

    예약하기 기능

--- src/main/java/com/goormTable/main/controller/MainController.java
+++ src/main/java/com/goormTable/main/controller/MainController.java
@@ -1,4 +1,33 @@
 package com.goormTable.main.controller;
 
+import com.goormTable.login.dto.LoginDto;
+import com.goormTable.login.service.LoginService;
+import com.goormTable.main.service.MainService;
+import com.goormTable.member.dto.ReservationDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "MainController", description = "메인 페이지 API")
+@RestController
 public class MainController {
+    @Autowired
+    private MainService mainService;
+    //TODO 마스터계정만 멤버(가게사장님)를 등록할수 있게 필요
+    @Operation(summary = "예약하기", description = "예약할 데이터를 처리합니다.")
+    @PostMapping("/insReser")
+    public ResponseEntity<String> insMem(@RequestBody ReservationDto reservationDto) {
+        ResponseEntity responseEntity = mainService.registerReservation(reservationDto);
+
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return ResponseEntity.ok("예약 완료");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("예약 실패");
+        }
+    }
 }
--- src/main/java/com/goormTable/main/service/MainService.java
+++ src/main/java/com/goormTable/main/service/MainService.java
@@ -1,4 +1,36 @@
 package com.goormTable.main.service;
 
+import com.goormTable.member.dto.ReservationDto;
+import com.goormTable.member.entity.Member;
+import com.goormTable.member.entity.Reservation;
+import com.goormTable.member.repository.member.SpringDataJpaMemberRepository;
+import com.goormTable.member.repository.reservation.SpringDataJpaReservationRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class MainService {
+    private final SpringDataJpaReservationRepository reservationRepository;
+    private final SpringDataJpaMemberRepository memberRepository;
+    @Transactional
+    public ResponseEntity registerReservation(ReservationDto reservationDto){
+        Member member = memberRepository.findById(Long.valueOf(reservationDto.getMemberSeq()))
+                .orElseThrow(() -> new RuntimeException("Member not found"));
+
+        Reservation reservation = Reservation.builder()
+                .reservationTime(LocalDateTime.now())
+                .peopleNum(reservationDto.getPeopleNum())
+                .phoneNum(reservationDto.getPhoneNum())
+                .extra(reservationDto.getExtra())
+                .status("WAIT")
+                .member(member)
+                .build();
+
+        return ResponseEntity.ok(reservationRepository.save(reservation));
+    }
 }
